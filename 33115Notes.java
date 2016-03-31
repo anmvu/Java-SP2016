0695363a3e63d416d6af9311fb95c46319adbeec
annievuvu <anvu94@gmail.com>
2016-03-31 19:29:37 -0400

    merr notes

---
+++ 33115Notes.java
@@ -0,0 +1,30 @@
+
+//some internet stuff
+/*
+10001
+	- binary : 17
+	- 10,000 + 1
+	- 2's complement: -15
+	- F + 2
+
+	binary = data output stream - string of binary data
+	ascii - print output stream
+	unicode - print writer
+
+	data input stream
+
+sources:
+
+	file - file reader
+	char array - array reader
+	string stream - string reader
+	pipe - pipe reader - other systems and remote systems
+
+open system interconnect - iosi
+
+*/
+public class Java33116{
+	public static void main(String() args){
+
+	}
+}
---
+++ PlayingWithJava/src/playingwithjava/PlayingWithJava.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package playingwithjava;
+import java.util.*;
+
+/**
+ *
+ * @author webteam
+ */
+public class PlayingWithJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int x = 100;
+        int y = 200;
+        System.out.println("println x + y = "+x+y);
+        System.out.print("print x + y = " + x + y + "\n");
+        System.out.println("println (x+y) = " + (x+y));
+        System.out.print(x+y);
+        System.out.print("\n" + (x+y) + "\n");
+        System.out.print(x+y + "print\n");
+        System.out.println(x+y + " println x + y");
+        System.out.println(x+y);
+        Scanner sin = new Scanner(System.in);
+        int[] ar;
+        ArrayList<Double> k;
+        k = new ArrayList<Double>();
+        String i = "Yay";
+        String j = "Nah";
+        boolean equil = i.equals(j);
+        S
+    }
+    
+}
