5af5550f4b0aa1e28446383a2d48d54efd1815b9
An Vu <anvu94@gmail.com>
2016-04-28 16:59:41 -0400

    clock without the calendar

--- Clock/src/clock/Clock.java
+++ Clock/src/clock/Clock.java
@@ -21,7 +21,10 @@ public class Clock {
         JFrame jf = new JFrame("Clock");
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setSize(400,400);
-        GraphicsPanel c = new GraphicsPanel();
+        jf.setResizable(false);
+        double width = jf.getSize().getWidth();
+        double height = jf.getSize().getHeight();
+        GraphicsPanel c = new GraphicsPanel(width,height);
         new UpdatePanel(c).start();
         jf.add(c);
         jf.setVisible(true);
@@ -30,13 +33,14 @@ public class Clock {
 }
 
 class UpdatePanel extends Thread{
-    JPanel jp;
-    UpdatePanel(JPanel newjp){jp = newjp;}
+    GraphicsPanel jp;
+    UpdatePanel(GraphicsPanel newjp){jp = newjp;}
+    @Override
     public void run(){
         try{
             while(true){
-                jp.repaint();
-                sleep(100000);
+                jp.moveAngle();
+                sleep(100);
             }
         }catch(Exception e){}
     }
@@ -45,58 +49,109 @@ class UpdatePanel extends Thread{
 class Hand{
     int x;
     int y;
-    int angle;
-    int time;
 }
 
 class GraphicsPanel extends JPanel{
-    int i;
-    int hours;
-    int minutes;
-    int seconds;
-    Hand hour;
-    Hand minute;
-    Hand second;
-    GraphicsPanel(){
-        i = 0;
-        hours = 0;
-        minutes = 0;
-        seconds = 0;
-        int dist = 100;
-        for(seconds = 90; seconds < 450; seconds += 60){
-            
-            if(seconds == 90){
-                seconds = 0;
-                minutes++;
-                if(minutes == 60){
-                    minutes = 0;
-                    hours++;
-                    if(hours == 12){
-                        hours = 0;
-                    }
-                }
-            }
-            minute = new Hand(); 
-            minute.x = (int)(dist*Math.cos(Math.toRadians(minutes*6)));
-            minute.y = (int)(dist*Math.sin(Math.toRadians(minutes*6)));
-            hour = new Hand();
-            hour.x = (int)(dist*Math.cos(Math.toRadians(hours*30)));
-            hour.y = (int)(dist*Math.cos(Math.toRadians(hours*30)));
-            second = new Hand();
-            second.x = (int)(dist*Math.cos(Math.toRadians(seconds)));
-            second.y = (int)(dist*Math.sin(Math.toRadians(seconds)));
+    int hour;
+    int minute;
+    int second;
+    double length;
+    static double width;
+    static double height;
+    Hand[] hours;
+    Hand[] minutes;
+    Hand[] seconds;
+    GraphicsPanel(double newWidth, double newHeight){
+        hour = 0;
+        minute = 0;
+        second = 0;
+        width = newWidth;
+        height = newHeight;
+        hours = new Hand[24];
+        minutes = new Hand[60];
+        seconds = new Hand[600];
+        int j = 0;
+        int k = 0;
+        int l = 0;
+        int length = (int)height;
+        if (width < height) length = (int)width;
+        for(double i = 360; i > 0; i -= 0.6){
+            Hand sec = new Hand();
+            sec.x = (int)(length*0.45*Math.sin(Math.toRadians(i+180)));
+            sec.y = (int)(length*0.45*Math.cos(Math.toRadians(i+180)));
+            seconds[j++] = sec;
         }
+        for(int i = 360; i > 0; i -= 6){
+            Hand min = new Hand(); 
+            min.x = (int)(length*0.4*Math.sin(Math.toRadians(i+180)));
+            min.y = (int)(length*0.4*Math.cos(Math.toRadians(i+180)));
+            minutes[k++] = min;
+        }
+        for (int i = 360; i > 0; i -= 15){
+            Hand h = new Hand();
+            h.x = (int)(length*0.3*Math.sin(Math.toRadians(i+180)));
+            h.y = (int)(length*0.3*Math.cos(Math.toRadians(i+180)));
+            hours[l++] = h;
+        }
+        
     }
     
+    void moveAngle(){
+        if(second == 599){
+            second = 0;
+            minute++;
+            if((minute+1) % 30 == 0){
+                hour++;
+                if(minute == 59) minute = 0;
+            }
+            if(hour == 23) hour = 0;
+        }
+        else second++;
+        
+        repaint();
+    }
     
     @Override
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        double height = getSize().getHeight();
-        double width = getSize().getWidth();
+        int boxSize;
+        width = getSize().getWidth();
+        height = getSize().getHeight();
+        if (width < height) boxSize = (int)width;
+        else boxSize = (int)height;
         int middleWidth = (int)width/2;
         int middleHeight = (int)height/2;
-        int boxSize = 200;
-        g.drawRect(Hour.)
+        
+        g.setColor(Color.white);
+        g.fillOval(middleWidth-boxSize/2,middleHeight-boxSize/2, boxSize, boxSize);
+       
+        
+        Hand hour2 = new Hand();
+        Hand minute2 = new Hand();
+        Hand second2 = new Hand();
+        hour2.x = hours[hour].x + middleWidth;
+        hour2.y = hours[hour].y + middleHeight;
+        minute2.x = minutes[minute].x + middleWidth;
+        minute2.y = minutes[minute].y + middleHeight;
+        second2.x = seconds[second].x + middleWidth;
+        second2.y = seconds[second].y + middleHeight;
+        g.setColor(Color.green);
+        g.drawLine(hour2.x,hour2.y,middleWidth, middleHeight);
+        g.setColor(Color.red);
+        g.drawLine(minute2.x,minute2.y,middleWidth, middleHeight);
+        g.setColor(Color.blue);
+        g.drawLine (second2.x,second2.y,middleWidth, middleHeight);
+        
+        g.setColor(Color.black);
+        int h = 0;
+        for(int i = 0; i < 360; i += 30){
+            String time = Integer.toString(h);
+            if(h == 0) time = "12";
+            h++;
+            int x =(int) (boxSize*0.4*Math.cos(Math.toRadians(i-90)) + middleWidth-4);
+            int y = (int)(boxSize*0.4*Math.sin(Math.toRadians(i-90)) + middleHeight+5);
+            g.drawString(time,x ,y );
+        }
+   
     }
 }
